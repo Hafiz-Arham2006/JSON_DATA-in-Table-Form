001594a66c361ec1bf075dad6c4e29cdde9e6090
HafizArham290 <hafizarham2006@gmail.com>
2023-12-01 23:20:30 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+let data =[
+    {
+        "Name":"Abdul Wahab Amir",
+        "Father_Name": "Muhammad Amir",
+        "Education" : "Bachelors",
+        "Roll_No" :"420",
+        "Profile_Picture": "image/i1.jpg"
+    },
+{
+    "Name":"Muhammad Arham",
+    "Father_Name": "Muhammad Asif",
+    "Education" : "Matric",
+    "Roll_No" :"1478330",
+    "Profile_Picture": "image/i2.jpg"
+},
+{
+    "Name":"Muhammad Ibrahim",
+    "Father_Name": "Muhammad Yaqoob",
+    "Education" : "Graduation",
+    "Roll_No" :"14783123",
+    "Profile_Picture": "image/i3.jpg"
+},
+{
+    "Name":"Muhammad Ahmed",
+    "Father_Name": "Muhammad Ashfaq",
+    "Education" : "Inter",
+    "Roll_No" :"121313",
+    "Profile_Picture": "image/i4.jpg"
+},
+{
+    "Name":"Muhammad Shafiq",
+    "Father_Name": "Muhammad Younus",
+    "Education" : "Matric",
+    "Roll_No" :"57643",
+    "Profile_Picture": "image/i5.jpg"
+},
+
+
+
+
+
+
+
+]
